bd873082b843e5a078de17d570301ce42c7157a1
Sheil Sarda <sheilsarda@yahoo.com>
2021-01-30 23:01:32 -0500

    20Hz done

--- MEAM510_Lab1/blink_led/src/main.c
+++ MEAM510_Lab1/blink_led/src/main.c
@@ -4,27 +4,29 @@
 
 #include "teensy_general.h"  // includes the resources included in the teensy_general.h file
 
-#define FREQ_HZ 200 // variable for frequency
-#define PRESCALAR 64
+#define FREQ_HZ 20 // variable for frequency
+#define PRESCALAR 256 // prescalar used
+#define SYS_CLOCK 16e6 // clock speed (16 Mhz)
 int main(void)
 {
     /* insert your hardware initialization here */
 
     teensy_clockdivide(0); //set the clock speed
     set(DDRC, 7);
-    // set(TCCR1B, CS12); // set 256 prescalar
+  
+    set(TCCR1B, CS12); // set 256 prescalar
+    
+    // set(TCCR1B, CS11); // 8 prescalar
 
-    // 64 prescalar
-    set(TCCR1B, CS10); 
-    set(TCCR1B, CS11); 
+    TCNT1 = 0x00;
+    long cutoff = SYS_CLOCK/(2*FREQ_HZ*PRESCALAR); 
 
-    uint64_t cutoff = 16e6 / (FREQ_HZ * PRESCALAR);
     for(;;){
 	
 	if(TCNT1 > cutoff){
             toggle(PORTC, 7);
-	    // teensy_led(TOGGLE);
-	    TCNT1 = 0;
+	    teensy_led(TOGGLE);
+	    TCNT1 = 0x00;
         }
     }
     return 0;   /* never reached */
