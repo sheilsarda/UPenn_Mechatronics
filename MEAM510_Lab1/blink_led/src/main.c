c05d98e29c8a1e24bda05bbe6cd86b8ff7908205
Sheil Sarda <sheilsarda@yahoo.com>
2021-01-31 13:44:28 -0500

    finished 1.3.3 pwm

--- MEAM510_Lab1/blink_led/src/main.c
+++ MEAM510_Lab1/blink_led/src/main.c
@@ -4,30 +4,34 @@
 
 #include "teensy_general.h"  // includes the resources included in the teensy_general.h file
 
-#define FREQ_HZ 20 // variable for frequency
-#define PRESCALAR 256 // prescalar used
-#define SYS_CLOCK 16e6 // clock speed (16 Mhz)
-#define DUTY_CYCLE 100 // duty cycle %
+#define FREQ_HZ    1    // variable for frequency
+#define PRESCALAR  256  // prescalar used
+#define SYS_CLOCK  16e6 // clock speed (16 Mhz)
+#define DUTY_CYCLE 0.5  // duty cycle %
 
 int main(void)
 {
-    /* insert your hardware initialization here */
-
+    set(TCCR1B, CS12); // set 256 prescalar    
     teensy_clockdivide(0); //set the clock speed
-    set(DDRC, 7);
-    set(TCCR1B, CS12); // set 256 prescalar
-    
-    TCNT1 = 0x00;
+
+    set(DDRB, 5); // B5 is output compare pin
+
+    // (mode 14) UP to ICR1, PWM mode
+    set(TCCR1B, WGM13); set(TCCR1B, WGM12); set(TCCR1A, WGM11);
+
+    // toggle B5 at OC
+    set(TCCR1A, COM1A1); 
 
     // set compare match register
-    OC1A = SYS_CLOCK/(2*FREQ_HZ*PRESCALAR); 
-    for(;;){
-	
-	if(TCNT1 > cutoff){
-            toggle(PORTC, 7);
-	    teensy_led(TOGGLE);
-	    TCNT1 = 0x00;
-        }
-    }
+    ICR1  = SYS_CLOCK/(FREQ_HZ*PRESCALAR); 
+    // set duty cycle PWM
+    OCR1A = ICR1*DUTY_CYCLE;  
+
+    /* Set OC1A on compare match when upcounting. Clear OC1A 
+     * on compare match when down-counting. 
+     */
+    set(TCCR1B, WGM13); set(TCCR1B, WGM12);
+   
+    while(1);
     return 0;   /* never reached */
 }
