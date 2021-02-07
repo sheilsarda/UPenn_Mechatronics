2de369d73db6a64179f48dc50e723c2c30705270
Sheil Sarda <sheilsarda@yahoo.com>
2021-02-06 19:42:17 -0500

    done with 2.1.2 code

--- MEAM510_Lab2/teensy_lab2/src/main.c
+++ MEAM510_Lab2/teensy_lab2/src/main.c
@@ -6,10 +6,42 @@
 #include "t_usb.h"
 #include <stdbool.h>
 
-void toggle_print(){
-    m_usb_tx_uint(TCNT3);
-    m_usb_tx_string(" >> Switch Toggled\r\n");
+#define DEBUG_PRINTS 1
 
+
+int button_presses[5]; // record tcnt3 values for each press
+int press_i; // ix of button press being recorded
+bool prev_state; // true when switch is depressed
+
+void compute_results(){
+    unsigned long sum_t = 0;
+    for(int i = 1; i < 5; ++i){
+        sum_t += (button_presses[i] - button_presses[i-1]);
+    }
+    sum_t /= 4;
+
+    m_usb_tx_string("=========================\r\n");
+    m_usb_tx_string("|| Reaction Time: "); 
+    m_usb_tx_ulong(sum_t);
+    m_usb_tx_string("   ||\r\n");
+    m_usb_tx_string("=========================\r\n");
+
+}
+
+void toggle_print(){
+    m_usb_tx_int(press_i);
+    m_usb_tx_string(" >> Button Press Recorded\r\n");
+
+}
+
+void record_press(bool new_state){
+
+    #ifdef DEBUG_PRINTS
+    if(prev_state != new_state) toggle_print();
+    #endif
+
+    prev_state = new_state;
+    button_presses[press_i++] = TCNT3;
 }
 
 int main(void){
@@ -26,19 +58,29 @@ int main(void){
     set(DDRC, 7);  // C7 is output 
     clear(PORTC, 7); // no LED
 
-    bool prev_state = false; // true when switch is depressed
+    prev_state = false;
 
+    #ifdef DEBUG_PRINTS
     while(!m_usb_isconnected()); // wait for a connection
+    #endif
+    
     while(1){
+	
+	if(press_i = 4) compute_results();
+	if(press_i == 0 || press_i = 4){
+	    memset(button_presses, 0x00); // clear array
+	    press_i = 0;
+	}
+	if(press_i == 0) display_prompt();
+	
 	// detect if button is pressed
         if(!bit_is_set(PINB, 7)){
-            set(PORTC, 7);
+            record_press(true);
+	    set(PORTC, 7);
 	    teensy_led(ON);
-	    if(!prev_state) toggle_print();
-	    prev_state = true;
 	} else {
 	    // button is not pressed
-	    if(prev_state) toggle_print();
+	    record_press(false);
 	    clear(PORTC, 7);
 	    teensy_led(OFF);
 	    prev_state = false;
