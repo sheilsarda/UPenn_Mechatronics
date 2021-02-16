f15df286b08ad004b51d34d47a14df767b1769c7
Sheil Sarda <sheilsarda@yahoo.com>
2021-02-15 19:06:57 -0500

    2.3 now works

--- Lab2/teensy_lab2/src/main.c
+++ Lab2/teensy_lab2/src/main.c
@@ -2,56 +2,86 @@
  * Author: Sheil Sarda
  */
 #include "teensy_general.h"
+#include "t_usb.h"
 #include <stdbool.h>
+#include <string.h>
 
 #define CLOCK_SPEED 16e6
-#define PRESCALAR 1024
+#define PRESCALAR 1024 // freq of 15.625kHz
 #define SW_SIZE 800
+#define USB 0
+#define TOLERANCE 10.0
+#define FREQ1 23.0
+#define FREQ2 200.0
+#define TIMEOUT 0xFFFF
 
-bool sw[SW_SIZE];
-int num_entries = 0;
+float t = 0.0;
 
-bool update_window(bool is20){
+unsigned int signal_died;
 
-    if(num_entries < SW_SIZE) sw[num_entries - 1] = is20;
-    else {
-        num_entries = (num_entries % SW_SIZE);
-        sw[num_entries] = is20;
-    }
-    num_entries++;
+bool update_window(){
 
-    int num_true = 0;
-    for(int i = 0; i < SW_SIZE; ++i)
-        if(sw[i]) num_true++;
+    TCNT3 = 0x00; // waiting for next edge
+    signal_died = TIMEOUT; // stop looking for signal
+    while(!bit_is_set(PINB, 7) && signal_died) signal_died--;
+    while(bit_is_set(PINB, 7) && signal_died)  signal_died--;
 
-    if(num_true > SW_SIZE/2) return true;
+    t =  TCNT3;
+    t /= 1.0*CLOCK_SPEED;
+    t *= 1000.0*PRESCALAR; // period in ms
+
+    #ifdef USB 
+        m_usb_tx_string("Period was: ");
+        m_usb_tx_uint(t);
+        m_usb_tx_string("\r\n");
+    #endif
+    
+    if(t < 1000.0/(FREQ1 - TOLERANCE) && 
+       t > 1000.0/(FREQ1 + TOLERANCE)) return true;
     return false;
+
 }
 
 
 int main(void){
+    #ifdef USB
+        m_usb_init();
+        while(!m_usb_isconnected());
+    #endif
+
     // set 1024 prescalar
     set(TCCR3B, CS32); set(TCCR3B, CS30);
-
     teensy_clockdivide(0); //set the clock speed
 
     clear(DDRB, 7); // B7 is input
-    // set(PORTB, 7);  // internal pullup
+    set(PORTB, 7);  // internal pullup
 
     set(DDRC, 7); clear(PORTC, 7); // RED
     set(DDRB, 4); clear(PORTB, 4); // GREEN
 
     bool result = false;
-    memset(sw, 0, sizeof(sw));
+    bool is20   = false;
 
     while(1){
-        result = update_window(!bit_is_set(PINB, 7));
+        result = !bit_is_set(PINB, 7);
 
-        // detect freq
-        if(result){
+        if(!result){ // no signal
+            clear(PORTB, 4);
+            clear(PORTC, 7);
+            continue;
+        }
+        
+        is20 = update_window();
+        if(is20){
+            #ifdef USB 
+                m_usb_tx_string("Detecting 10-30Hz\r\n");
+            #endif
             set(PORTC, 7);
             clear(PORTB, 4);
         } else {
+            #ifdef USB 
+                m_usb_tx_string("Detecting >30Hz\r\n");
+            #endif
             clear(PORTC, 7);
             set(PORTB, 4);
         }
