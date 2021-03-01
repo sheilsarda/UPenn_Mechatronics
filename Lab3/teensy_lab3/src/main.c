da0b671e998fd13698afe3b5fc18693516e448ac
Sheil Sarda <sheilsarda@yahoo.com>
2021-02-28 22:25:19 -0500

    added adc configure subroutine

--- Lab3/teensy_lab3/src/main.c
+++ Lab3/teensy_lab3/src/main.c
@@ -10,6 +10,34 @@
 #define PRESCALAR 1024 // freq of 15.625kHz
 #define USB 1
 
+void setup_ADC(char adc_num){
+
+    set(ADMUX, REFS0); // AVcc
+    set(ADCSRA, ADEN); // enable ADC
+    set(ADCSRA, ADSC); // first conversion
+
+    // 128 ADC prescalar
+    set(ADCSRA, ADPS0); 
+    set(ADCSRA, ADPS1); 
+    set(ADCSRA, ADPS2); 
+
+    switch(adc_num){
+        case 0d00: set(DIDR0, ADC0D); break;
+        case 0d01: set(DIDR0, ADC1D); break;
+        case 0d04: set(DIDR0, ADC4D); break;
+        case 0d05: set(DIDR0, ADC5D); break;
+        case 0d06: set(DIDR0, ADC6D); break;
+        case 0d07: set(DIDR0, ADC7D); break;
+        case 0d08: set(DIDR2, ADC8D); break;
+        case 0d09: set(DIDR2, ADC9D); break;
+        case 0d10: set(DIDR2, ADC10D); break;
+        case 0d11: set(DIDR2, ADC11D); break;
+        case 0d12: set(DIDR2, ADC12D); break;
+        case 0d13: set(DIDR2, ADC13D); break;
+    }
+    
+}
+
 int main(void){
     #ifdef USB
         m_usb_init();
@@ -20,6 +48,7 @@ int main(void){
     set(TCCR3B, CS32); set(TCCR3B, CS30);
     teensy_clockdivide(0); //set the clock speed
 
+    setup_ADC(); 
 
     while(1){
     
