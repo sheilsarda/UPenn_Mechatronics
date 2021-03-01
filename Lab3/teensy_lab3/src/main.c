008e618a64088cbf5ffd52fe7270afd75326c014
Sheil Sarda <sheilsarda@yahoo.com>
2021-03-01 12:20:01 -0500

    added channel selector

--- Lab3/teensy_lab3/src/main.c
+++ Lab3/teensy_lab3/src/main.c
@@ -21,21 +21,46 @@ void setup_ADC(char adc_num){
     set(ADCSRA, ADPS1); 
     set(ADCSRA, ADPS2); 
 
-    switch(adc_num){
-        case 0d00: set(DIDR0, ADC0D); break;
-        case 0d01: set(DIDR0, ADC1D); break;
-        case 0d04: set(DIDR0, ADC4D); break;
-        case 0d05: set(DIDR0, ADC5D); break;
-        case 0d06: set(DIDR0, ADC6D); break;
-        case 0d07: set(DIDR0, ADC7D); break;
-        case 0d08: set(DIDR2, ADC8D); break;
-        case 0d09: set(DIDR2, ADC9D); break;
-        case 0d10: set(DIDR2, ADC10D); break;
-        case 0d11: set(DIDR2, ADC11D); break;
-        case 0d12: set(DIDR2, ADC12D); break;
-        case 0d13: set(DIDR2, ADC13D); break;
+    switch(adc_num){ // Disable digital input
+        case 0d00: set(DIDR0, ADC0D);   break;
+        case 0d01: set(DIDR0, ADC1D);   break;
+        case 0d04: set(DIDR0, ADC4D);   break;
+        case 0d05: set(DIDR0, ADC5D);   break;
+        case 0d06: set(DIDR0, ADC6D);   break;
+        case 0d07: set(DIDR0, ADC7D);   break;
+        case 0d08: set(DIDR2, ADC8D);   break;
+        case 0d09: set(DIDR2, ADC9D);   break;
+        case 0d10: set(DIDR2, ADC10D);  break;
+        case 0d11: set(DIDR2, ADC11D);  break;
+        case 0d12: set(DIDR2, ADC12D);  break;
+        case 0d13: set(DIDR2, ADC13D);  break;
     }
-    
+
+    switch(adc_num){
+        case 0d00:                      break;
+        case 0d01:  set(ADMUX, MUX0);   break;
+        case 0d04:  set(ADMUX, MUX2);   break;
+        case 0d05:  set(ADMUX, MUX2); 
+                    set(ADMUX, MUX0);   break;
+        case 0d06:  set(ADMUX, MUX2);
+                    set(ADMUX, MUX1);   break;
+        case 0d07:  set(ADMUX, MUX0);
+                    set(ADMUX, MUX1);
+                    set(ADMUX, MUX2);   break;
+        case 0d08:  set(ADCSRB, MUX5);  break;
+        case 0d09:  set(ADCSRB, MUX5);
+                    set(ADMUX, MUX0);   break;
+        case 0d10:  set(ADCSRB, MUX5);
+                    set(ADMUX, MUX1);   break;
+        case 0d11:  set(ADCSRB, MUX5);
+                    set(ADMUX, MUX1);
+                    set(ADMUX, MUX0);   break;
+        case 0d12:  set(ADCSRB, MUX5);
+                    set(ADMUX, MUX2);   break;
+        case 0d13:  set(ADCSRB, MUX5);
+                    set(ADMUX, MUX2);
+                    set(ADMUX, MUX0);   break;
+    }                    
 }
 
 int main(void){
